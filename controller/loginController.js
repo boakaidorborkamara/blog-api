927a6214b2dd81d8f64f91536a588c9f49a09e1c
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-07-08 14:14:22 +0000

    implement login

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require("../utils/config");
 const User = require("../model/userModel");
 
 const logIn = (req, res, next)=>{
@@ -14,7 +16,7 @@ const logIn = (req, res, next)=>{
     //     return res.status(400).json({success:false, message:"password are required!", data:null})
     // }
 
-    User.findOne({username:username})
+    User.findOne({username:username}).lean()
     .then(user =>{
 
         console.log("getting user");
@@ -34,7 +36,13 @@ const logIn = (req, res, next)=>{
             }
 
             // handle correct password 
-            res.status(200).json({msg:"Logging in..."})
+            delete user.password;
+            console.log("user", user)
+            let token = jwt.sign({ username: user.username, id:user.id }, JWT_SECRET, { expiresIn: '24h' });
+
+            console.log(typeof user)
+            
+            res.status(200).json({success:true, msg:"Login successful", token:token, data: user});
         });
     }) 
 
--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,10 @@
 let PORT = 3000 || process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 module.exports = {
     PORT,
-    MONGO_URL 
+    MONGO_URL,
+    JWT_SECRET
 } 
