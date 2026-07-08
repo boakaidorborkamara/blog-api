f8334c0af361fbde67566401d5b24e91bb230acc
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-07-08 00:18:20 +0000

    implement hashing of user password

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,38 @@
+const bcrypt = require('bcrypt');
 const User = require("../model/userModel");
 const logger = require("../utils/logger");
+
 
 const addUser = (req, res, next)=>{
     logger.info("Adding a new user...");
 
     let {username, name, password} = req.body;
 
-    let new_user = new User({username, name, password});
+    // hash password 
+    const saltRounds = 10;
+    const myPlaintextPassword = password;
+    let hashPassword;
+    
+    bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+        // create hash password 
+        password = hash;
 
-    new_user.save()
-    .then(blog =>{
-        logger.info("user added", new_user)
-        res.status(201).json({success:true, message:"User created successfully!", data: new_user})
+        // create new user after hashing pw 
+        let new_user = new User({username, name, password});
+
+        // save user to db 
+        new_user.save()
+        .then(blog =>{
+            logger.info("user added", new_user)
+            res.status(201).json({success:true, message:"User created successfully!", data: new_user})
+        })
+        .catch(err => next(err));
+
     })
-    .catch(err => next(err));
+    .catch(err=> next(err));
+
+   
+   
 }
 
 const getUsers = (req, res, next)=>{
